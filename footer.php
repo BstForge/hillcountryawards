7641927499ba0309eb6b08bd8c36acdf4f083cda
BstForge <157843230+BstForge@users.noreply.github.com>
2025-08-23 07:48:47 -0500

    Remove binary logo placeholder and use text logo

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <p>Hill Country Awards &amp; Trophies</p>
+  <p>123 Placeholder St, Hill Country, TX 00000</p>
+  <p>Phone: (000) 000-0000 | Email: info@hillcountryawards.com</p>
+  <div class="social-icons">
+    <span>FB</span>
+    <span>TW</span>
+    <span>IG</span>
+  </div>
+</footer>
